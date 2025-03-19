e534ff039854bc982ddbbdf52324c42c43d43aa9
richard <richard.wasonga@student.moringaschool.com>
2025-03-19 11:53:34 +0300

    complete lab

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
   .then(res => res.json())
   .then(data => renderBooks(data))
-  .catch(error => console.error("Error fetching books:", error));
 }
 
 function renderBooks(books) {
